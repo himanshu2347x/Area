724d2fdbef9e1e144a96db4ab9c5a664fc5818a3
himanshu2347x <himanshu.sinha392@gmail.com>
2025-10-16 16:14:59 +0530

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./docs/**/*.{html,js}"],
+  theme: {
+    screens: {
+      lg: '1280px',
+      md: '800px',
+      sm: '375px',
+    },
+    extend: {
+      fontFamily: {
+        dm: ['"DM Sans"', 'sans-serif'],
+        crimson: ['"Crimson Text"', 'serif'],
+        roboto: ['"Roboto Mono"', 'monospace'],
+        rethink: ['"Rethink Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
